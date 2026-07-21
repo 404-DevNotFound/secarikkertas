943768c5e339c9a2bb0388d8a683d0568ba7aaf4
404-DevNotFound <dante.engineer@gmail.com>
2026-07-21 19:55:09 +0700

    ubah footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="mt-12 py-6 text-center text-sm text-slate-400 border-t">
-      © {new Date().getFullYear()} secarikkertas.com —  Ruang untuk berbagi cerita.
+      © {new Date().getFullYear()} secarikkertas.com | Bukan sekedar komunitas biasa melainkan ruang untuk bercerita
     </footer>
   )
 }
